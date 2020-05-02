d315a009afa3a64e2db7f6b943ea4bd0429067a1
Aditya Sirish <aditya@saky.in>
2020-05-01 20:21:37 -0400

    Add diff and ownership functionality; use CLI commands
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- stat-script.py
+++ stat-script.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-import sys
 import os
 import json
+import argparse
+import subprocess
 
 
 EXCLUDE_PATHS = ('proc', 'sys', 'usr/local/stat-script', 'usr/bin/stat-script')
+DIFF_EXCLUDE_PATHS = ('/build/', '/srcdest/', '/startdir/', '/logdest/')
+
+
+def _dump_target(target, target_path):
+    with open(target_path, 'w') as f:
+        json.dump(target, f, indent=4)
 
 
 def _check_exclude(path):
@@ -16,14 +23,13 @@ def _check_exclude(path):
     return False
 
 
-def main():
+def record(root_path, target_path):
     file_stat = {}
-    path = sys.argv[1]
-    out = sys.argv[2]
-    print("Running stat-script...")
-    if os.path.isdir(path):
-        for root, dirs, files in os.walk(path, followlinks=False,
-                                         topdown=True):
+
+    print('Recording stat...')
+
+    if os.path.isdir(root_path):
+        for root, dirs, files in os.walk(root_path, followlinks=False, topdown=True):
             dirs[:] = [d for d in dirs if not d.startswith(EXCLUDE_PATHS)]
             dirpaths = []
             for dirname in dirs:
@@ -41,14 +47,86 @@ def main():
             for filepath in filepaths:
                 if not _check_exclude(filepath):
                     file_stat[filepath] = os.stat(filepath).st_atime
-    elif os.path.isfile(path):
-        absolute_path = os.path.abspath(path)
+    elif os.path.isfile(root_path):
+        absolute_path = os.path.abspath(root_path)
         if not _check_exclude(absolute_path):
-            file_stat[absolute_path] = os.stat(path).st_atime
+            file_stat[absolute_path] = os.stat(root_path).st_atime
 
-    with open(out, 'w') as f:
-        json.dump(file_stat, f, indent=4)
+    _dump_target(file_stat, target_path)
 
 
-if __name__ == "__main__":
+def diff(pre_path, post_path, target_path):
+    diff_values = {}
+
+    print('Diff-ing pre and post stat values...')
+
+    with open(pre_path) as fp:
+        pre_data = json.load(fp)
+
+    with open(post_path) as fp:
+        post_data = json.load(fp)
+
+    for path in post_data:
+        if path.startswith(DIFF_EXCLUDE_PATHS):
+            continue
+        if path in pre_data:
+            if pre_data[path] < post_data[path]:
+                diff_values[path] = (pre_data[path], post_data[path])
+        else:
+            diff_values[path] = (None, post_data[path])
+
+    _dump_target(diff_values, target_path)
+
+
+def find_owners(diff_file_path, target_path):
+    ownership = {}
+
+    print('Finding owners...')
+
+    with open(diff_file_path) as fp:
+        diff_data = json.load(fp)
+
+    for path in diff_data:
+        try:
+            output = subprocess.check_output(['pacman', '-Qo', path])
+            package, version = output.decode('utf-8').split('owned by')[1].strip().split(' ')
+            ownership[path] = (package, version)
+        except subprocess.CalledProcessError:
+            ownership[path] = 'UNKNOWN'
+
+    _dump_target(ownership, target_path)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Granular stats for better provenance')
+
+    parser.add_argument('-r', '--record', action='store_true', dest='record_flag')
+    parser.add_argument('--root', type=str, dest='root_path')
+
+
+    parser.add_argument('-d', '--diff', action='store_true', dest='diff_flag')
+    parser.add_argument('--pre', type=str, dest='pre_path')
+    parser.add_argument('--post', type=str, dest='post_path')
+
+    parser.add_argument('-o', '--find-owners', action='store_true', dest='find_owners_flag')
+    parser.add_argument('--diff-file', type=str, dest='diff_file_path')
+
+    parser.add_argument('-t', '--target', type=str, dest='target_path')
+
+    args = parser.parse_args()
+
+    if not args.record_flag and not args.diff_flag and not args.find_owners_flag:
+        raise RuntimeError('select an operation')
+
+    if args.record_flag:
+        record(args.root_path, args.target_path)
+
+    if args.diff_flag:
+        diff(args.pre_path, args.post_path, args.target_path)
+
+    if args.find_owners_flag:
+        find_owners(args.diff_file_path, args.target_path)
+
+
+if __name__ == '__main__':
     main()
